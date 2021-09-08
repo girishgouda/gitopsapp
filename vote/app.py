5586a447f6d23120bb8614fa17d40a6a86cd1361
Girish <girishgouda@hotmail.com>
2021-09-08 11:53:00 +0530

    Update app.py

--- vote/app.py
+++ vote/app.py
@@ -7,7 +7,7 @@ import json
 import logging
 
 option_a = os.getenv('OPTION_A', "Dogs")
-option_b = os.getenv('OPTION_B', "Elephants")
+option_b = os.getenv('OPTION_B', "Cats")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
