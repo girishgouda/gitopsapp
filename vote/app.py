97f0d3e428f00e361f19de373fbec886635aa038
Girish <girishgouda@hotmail.com>
2021-09-08 15:16:18 +0530

    Update app.py

--- vote/app.py
+++ vote/app.py
@@ -6,7 +6,7 @@ import random
 import json
 import logging
 
-option_a = os.getenv('OPTION_A', "Devops")
+option_a = os.getenv('OPTION_A', "Devops123")
 option_b = os.getenv('OPTION_B', "Git")
 hostname = socket.gethostname()
 
